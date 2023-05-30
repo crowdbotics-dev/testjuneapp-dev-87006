42dd8c7e1bb4f73fdc70c68ebf052cb4dae4cf02
Ravinder sales <ravinder.y@crowdbotics.com>
2023-05-30 11:44:58 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,20 +1,39 @@
+import { Text } from "react-native";
+import { ImageBackground } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled2 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}><ImageBackground style={styles.aseHMIAt} source={require("./Dragon3.jpg")} resizeMode="cover"></ImageBackground><Text style={styles.yxQEQInv}>Lorem ipsum…</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  aseHMIAt: {
+    width: 194,
+    height: 132,
+    position: "absolute",
+    left: 66,
+    top: 53
+  },
+  yxQEQInv: {
+    width: 208,
+    height: 129,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    left: 54,
+    top: 237
   }
 });
 export default Untitled2;
