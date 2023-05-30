6e56cb3c2202ab8785ab7ffa03404371ed89b9db
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-30 05:36:14 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -20,6 +20,5 @@ class User(AbstractUser):
     """
     name = models.CharField(null=True,blank=True,max_length=255,)
     testingworld = models.BigIntegerField(null=True,blank=True,)
-    testworkdatamodel = models.BigIntegerField(null=True,blank=True,)
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
